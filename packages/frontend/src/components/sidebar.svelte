274cb4da125c7d92aaca8572eefd4772bda3555b
Gjorgji Kjosev <gorgi.kosev@gmail.com>
2023-09-20 22:34:08 +0100

    add frontend with skeleton ui

---
+++ packages/frontend/src/components/sidebar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts" context="module">
+  export interface Category {
+    name: string;
+    link?: string;
+    icon?: string;
+    children?: Category[];
+  };
+</script>
+
+<script lang="ts">
+  export let categories: Category[]
+</script>
+
+<nav class="list-nav">
+  <ul>
+    {#each categories as category}
+      <li><a href={category.link}>{category.name}</a></li>
+    {/each}
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About</a></li>
+  </ul>
+</nav>
---
+++ packages/frontend/src/routes/+layout.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+
+  let ApiUrl = import.meta.env.VITE_PUBLIC_API_URL;
+</script>
+
+<AppShell slotSidebarLeft="bg-surface-100-800-token w-0 lg:w-64 p-3">
+  <svelte:fragment slot="header">
+    <AppBar padding="py-3 px-4">
+      <svelte:fragment slot="lead">
+
+      <button class="lg:hidden btn btn-sm mr-4">
+        <span>
+            <svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+                <rect width="100" height="20" />
+                <rect y="30" width="100" height="20" />
+                <rect y="60" width="100" height="20" />
+            </svg>
+        </span>
+      </button>
+      <div class="flex items-center">
+
+        <strong class="text-xl uppercase">Tinier RSS</strong>
+      </div>
+
+      </svelte:fragment>
+
+	    <svelte:fragment slot="trail">
+        action buttons
+
+      </svelte:fragment>
+
+    </AppBar>
+
+  </svelte:fragment>
+  <svelte:fragment slot="sidebarLeft">
+    <!-- Insert the list: -->
+    <nav class="list-nav">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+    </nav>
+    <!-- --- -->
+  </svelte:fragment>
+
+	<slot />
+</AppShell>
