ba204d96d0512b6ef488c9e9e7a2f6569ee1932e
Gjorgji Kjosev <gorgi.kosev@gmail.com>
2023-09-20 23:17:38 +0100

    remove event bus

--- stacks/TinierRSS.ts
+++ stacks/TinierRSS.ts
@@ -1,38 +1,29 @@
-import { StackContext, Api, EventBus, StaticSite } from "sst/constructs";
+import { StackContext, Api, EventBus, StaticSite } from 'sst/constructs'
 
 export function TinierRSS({ stack }: StackContext) {
-
-
-  const bus = new EventBus(stack, "bus", {
-    defaults: {
-      retries: 10,
-    },
-  });
-
-  const api = new Api(stack, "api", {
+  const api = new Api(stack, 'api', {
     defaults: {
       function: {
-        bind: [bus],
+        timeout: 300,
       },
     },
     routes: {
-      "GET /sync": "packages/functions/src/sync.get",
-      "POST /sync": "packages/functions/src/sync.post"
+      'GET /sync': 'packages/functions/src/sync.get',
+      'POST /sync': 'packages/functions/src/sync.post',
     },
-  });
+  })
 
-  const web = new StaticSite(stack, "web", {
-    path: "packages/frontend",
-    buildOutput: "dist",
-    buildCommand: "npm run build",
+  const web = new StaticSite(stack, 'web', {
+    path: 'packages/frontend',
+    buildOutput: 'dist',
+    buildCommand: 'npm run build',
     environment: {
       VITE_PUBLIC_API_URL: api.url,
     },
-  });
-
+  })
 
   stack.addOutputs({
     ApiEndpoint: api.url,
     WebEndpoint: web.url,
-  });
+  })
 }
