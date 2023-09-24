0649e660ffe46a858fa07908714618e08dd8a1d6
Gjorgji Kjosev <gorgi.kosev@gmail.com>
2023-09-24 11:32:06 +0100

    feat: finalize watermelondb models

--- packages/frontend/src/model/index.ts
+++ packages/frontend/src/model/index.ts
@@ -1,6 +1,9 @@
-import { Database, Model, appSchema, tableSchema } from '@nozbe/watermelondb';
-import { schemaMigrations } from '@nozbe/watermelondb/Schema/migrations';
-import { field, json, text } from '@nozbe/watermelondb/decorators';
+import { Database, Model, appSchema, tableSchema } from '@nozbe/watermelondb'
+import { schemaMigrations } from '@nozbe/watermelondb/Schema/migrations'
+import { field, json, relation, text } from '@nozbe/watermelondb/decorators'
+import * as types from '@cloudy-rss/shared'
+
+function assertTypeExtends<_T extends U, U>() {}
 
 let FeedsTable = tableSchema({
 	name: 'feeds',
@@ -23,37 +26,39 @@ let FeedsTable = tableSchema({
 		{ name: 'createdAt', type: 'number' },
 		{ name: 'deleted', type: 'boolean' }
 	]
-});
+})
 
 class Feed extends Model {
-	static table = 'feeds';
+	static table = 'feeds'
 
-	@field('feedId') feedId!: string;
-	@text('title') title!: string;
-	@text('description') description!: string;
-	@json('image', (x) => x) image?: {
-		link: string;
-		title: string;
-		url: string;
-		description?: string;
-		height?: number;
-		width?: number;
-	};
-	@text('url') url!: string;
-	@text('author') author?: string;
-	@text('category') category?: string;
-	@field('state') state!: 'SYNCED' | 'SYNCING';
-	@field('lastBuildDate') lastBuildDate?: number;
-	@field('pubDate') pubDate?: number;
-	@field('skipDays') skipDays?: string[];
-	@field('skipHours') skipHours?: number[];
-	@field('ttl') ttl?: number;
-	@field('syncStartedAt') syncStartedAt?: number;
-	@field('syncCompletedAt') syncCompletedAt?: number;
-	@field('updatedAt') updatedAt!: number;
-	@field('createdAt') createdAt!: number;
-	@field('deleted') deleted!: boolean;
+	@field('feedId') feedId!: string
+	@text('title') title!: string
+	@text('description') description!: string
+	@json('image', x => x) image?: {
+		link: string
+		title: string
+		url: string
+		description?: string
+		height?: number
+		width?: number
+	}
+	@text('url') url!: string
+	@text('author') author?: string
+	@text('category') category?: string
+	@field('lastBuildDate') lastBuildDate?: number
+	@field('pubDate') pubDate?: number
+	@field('skipDays') skipDays?: string[]
+	@field('skipHours') skipHours?: number[]
+	@field('ttl') ttl?: number
+	@field('updatedAt') updatedAt!: number
+	@field('createdAt') createdAt!: number
+	@field('deleted') deleted!: boolean
 }
+
+type FeedType = Omit<Feed, keyof Model>
+
+assertTypeExtends<FeedType, types.Feed>()
+assertTypeExtends<types.Feed, FeedType>()
 
 let FeedItemsTable = tableSchema({
 	name: 'feedItems',
@@ -71,28 +76,33 @@ let FeedItemsTable = tableSchema({
 		{ name: 'createdAt', type: 'number' },
 		{ name: 'deleted', type: 'boolean' }
 	]
-});
+})
 
 class FeedItem extends Model {
-	static table = 'feedItems';
+	static table = 'feedItems'
 
-	@field('feedId') feedId!: string;
-	@field('pubDate') pubDate!: number;
-	@field('guid') guid!: string;
-	@text('title') title!: string;
-	@text('description') description!: string;
-	@text('author') author?: string;
-	@text('category') category?: string;
-	@text('link') link!: string;
-	@json('enclosure', (x) => x) enclosure?: {
-		type: string;
-		url: string;
-		length: number;
-	};
-	@field('updatedAt') updatedAt!: number;
-	@field('createdAt') createdAt!: number;
-	@field('deleted') deleted!: boolean;
+	@field('feedId') feedId!: string
+	@field('pubDate') pubDate!: number
+	@field('guid') guid!: string
+	@text('title') title!: string
+	@text('description') description!: string
+	@text('author') author?: string
+	@text('category') category?: string
+	@text('link') link!: string
+	@json('enclosure', x => x) enclosure?: {
+		type: string
+		url: string
+		length: number
+	}
+	@field('updatedAt') updatedAt!: number
+	@field('createdAt') createdAt!: number
+	@field('deleted') deleted!: boolean
 }
+
+type FeedItemType = Omit<FeedItem, keyof Model>
+
+assertTypeExtends<FeedItemType, types.FeedItem>()
+assertTypeExtends<types.FeedItem, FeedItemType>()
 
 let UserSubscriptionsTable = tableSchema({
 	name: 'userSubscriptions',
@@ -104,18 +114,24 @@ let UserSubscriptionsTable = tableSchema({
 		{ name: 'updatedAt', type: 'number' },
 		{ name: 'deleted', type: 'boolean' }
 	]
-});
+})
 
 class UserSubscription extends Model {
-	static table = 'userSubscriptions';
+	static table = 'userSubscriptions'
 
-	@field('feedId') feedId!: string;
-	@field('userId') userId!: string;
-	@field('requestedFrequency') requestedFrequency!: number;
-	@field('updatedAt') updatedAt!: number;
-	@field('createdAt') createdAt!: number;
-	@field('deleted') deleted!: boolean;
+	@field('feedId') feedId!: string
+	@field('userId') userId!: string
+	@field('url') url!: string
+	@field('requestedFrequency') requestedFrequency!: number
+	@field('updatedAt') updatedAt!: number
+	@field('createdAt') createdAt!: number
+	@field('deleted') deleted!: boolean
 }
+
+type UserSubscriptionType = Omit<UserSubscription, keyof Model>
+
+assertTypeExtends<UserSubscriptionType, types.UserSubscription>()
+assertTypeExtends<types.UserSubscription, UserSubscriptionType>()
 
 let UserFeedItemReadsTable = tableSchema({
 	name: 'userFeedItemReads',
@@ -126,30 +142,30 @@ let UserFeedItemReadsTable = tableSchema({
 		{ name: 'createdAt', type: 'number' },
 		{ name: 'updatedAt', type: 'number' }
 	]
-});
+})
 
 class UserFeedItemRead extends Model {
-	static table = 'userFeedItemReads';
+	static table = 'userFeedItemReads'
 
-	@field('userId') userId!: string;
-	@field('feedItemId') feedItemId!: string;
-	@field('deleted') deleted!: boolean;
-	@field('createdAt') createdAt!: number;
-	@field('updatedAt') updatedAt!: number;
+	@field('userId') userId!: string
+	@field('feedItemId') feedItemId!: string
+	@field('deleted') deleted!: boolean
+	@field('createdAt') createdAt!: number
+	@field('updatedAt') updatedAt!: number
 }
 
 export let schema = appSchema({
 	version: 1,
 	tables: [FeedsTable, FeedItemsTable, UserSubscriptionsTable, UserFeedItemReadsTable]
-});
+})
 
 export let migrations = schemaMigrations({
 	migrations: [
 		// We'll add migration definitions here later
 	]
-});
+})
 
-import LokiJSAdapter from '@nozbe/watermelondb/adapters/lokijs';
+import LokiJSAdapter from '@nozbe/watermelondb/adapters/lokijs'
 
 const adapter = new LokiJSAdapter({
 	schema,
@@ -160,10 +176,10 @@ const adapter = new LokiJSAdapter({
 	dbName: 'cloudyRSS',
 
 	// --- Optional, but recommended event handlers:
-	onQuotaExceededError: (error) => {
+	onQuotaExceededError: error => {
 		// Browser ran out of disk space -- TODO: setup event bus
 	},
-	onSetUpError: (error) => {
+	onSetUpError: error => {
 		// Database failed to load -- TODO: setup event bus
 	},
 	extraIncrementalIDBOptions: {
@@ -177,21 +193,20 @@ const adapter = new LokiJSAdapter({
 			// database was deleted in another browser tab (user logged out), so we must make sure we
 			// delete it in this tab as well - usually best to just refresh the page
 			// if (checkIfUserIsLoggedIn()) {
-			window.location.reload();
+			window.location.reload()
 			// }
 		}
 	}
-});
+})
 
 export let db = new Database({
 	adapter,
 	modelClasses: [Feed, FeedItem, UserSubscription, UserFeedItemRead]
-});
+})
 
-db.collections
-	.get('feeds')
-	.query()
-	.observe()
-	.subscribe((feeds) => feeds[0].);
-
-
+export let m = {
+	feeds: db.collections.get<Feed>('feeds'),
+	feedItems: db.collections.get<FeedItem>('feedItems'),
+	userSubscriptions: db.collections.get<UserSubscription>('userSubscriptions'),
+	userFeedItemReads: db.collections.get<UserFeedItemRead>('userFeedItemReads')
+}
