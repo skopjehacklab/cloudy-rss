3f908c5051a3361a4fae2ee5b4364fdcd897b060
Gjorgji Kjosev <gorgi.kosev@gmail.com>
2023-09-26 21:16:45 +0100

    chore: prettier

--- packages/frontend/src/database/index.ts
+++ packages/frontend/src/database/index.ts
@@ -12,56 +12,56 @@ type RelationKeys<T extends Model> = keyof T & `rel${string}`
 type OmitUnrelated<T extends Model> = Omit<T, keyof Model | RelationKeys<T>>
 
 let FeedsTable = tableSchema({
-	name: 'feeds',
-	columns: [
-		{ name: 'feedId', type: 'string', isIndexed: true },
-		{ name: 'title', type: 'string' },
-		{ name: 'description', type: 'string' },
-		{ name: 'url', type: 'string' },
-		{ name: 'author', type: 'string', isOptional: true },
-		{ name: 'category', type: 'string', isOptional: true },
-		{ name: 'lastBuildDate', type: 'number', isOptional: true },
-		{ name: 'pubDate', type: 'number', isOptional: true },
-		{ name: 'skipDays', type: 'string', isOptional: true },
-		{ name: 'skipHours', type: 'string', isOptional: true },
-		{ name: 'ttl', type: 'number', isOptional: true },
-		{ name: 'syncStartedAt', type: 'number', isOptional: true },
-		{ name: 'syncCompletedAt', type: 'number', isOptional: true },
-		{ name: 'state', type: 'string' },
-		{ name: 'updatedAt', type: 'number' },
-		{ name: 'createdAt', type: 'number' },
-		{ name: 'deleted', type: 'boolean' }
-	]
+  name: 'feeds',
+  columns: [
+    { name: 'feedId', type: 'string', isIndexed: true },
+    { name: 'title', type: 'string' },
+    { name: 'description', type: 'string' },
+    { name: 'url', type: 'string' },
+    { name: 'author', type: 'string', isOptional: true },
+    { name: 'category', type: 'string', isOptional: true },
+    { name: 'lastBuildDate', type: 'number', isOptional: true },
+    { name: 'pubDate', type: 'number', isOptional: true },
+    { name: 'skipDays', type: 'string', isOptional: true },
+    { name: 'skipHours', type: 'string', isOptional: true },
+    { name: 'ttl', type: 'number', isOptional: true },
+    { name: 'syncStartedAt', type: 'number', isOptional: true },
+    { name: 'syncCompletedAt', type: 'number', isOptional: true },
+    { name: 'state', type: 'string' },
+    { name: 'updatedAt', type: 'number' },
+    { name: 'createdAt', type: 'number' },
+    { name: 'deleted', type: 'boolean' }
+  ]
 })
 
 export class Feed extends Model {
-	static table = 'feeds'
+  static table = 'feeds'
 
-	@field('feedId') feedId!: string
-	@text('title') title!: string
-	@text('description') description!: string
-	@json('image', x => x) image?: {
-		link: string
-		title: string
-		url: string
-		description?: string
-		height?: number
-		width?: number
-	}
-	@text('url') url!: string
-	@text('author') author?: string
-	@text('category') category?: string
-	@field('lastBuildDate') lastBuildDate?: number
-	@field('pubDate') pubDate?: number
-	@field('skipDays') skipDays?: string[]
-	@field('skipHours') skipHours?: number[]
-	@field('ttl') ttl?: number
-	@field('updatedAt') updatedAt!: number
-	@field('createdAt') createdAt!: number
-	@field('deleted') deleted!: boolean
+  @field('feedId') feedId!: string
+  @text('title') title!: string
+  @text('description') description!: string
+  @json('image', x => x) image?: {
+    link: string
+    title: string
+    url: string
+    description?: string
+    height?: number
+    width?: number
+  }
+  @text('url') url!: string
+  @text('author') author?: string
+  @text('category') category?: string
+  @field('lastBuildDate') lastBuildDate?: number
+  @field('pubDate') pubDate?: number
+  @field('skipDays') skipDays?: string[]
+  @field('skipHours') skipHours?: number[]
+  @field('ttl') ttl?: number
+  @field('updatedAt') updatedAt!: number
+  @field('createdAt') createdAt!: number
+  @field('deleted') deleted!: boolean
 
-	@relation('feedItems', 'feedId') relFeedItems!: FeedItem[]
-	@relation('userSubscription', 'feedId') relUserSubscription!: UserSubscription
+  @relation('feedItems', 'feedId') relFeedItems!: FeedItem[]
+  @relation('userSubscription', 'feedId') relUserSubscription!: UserSubscription
 }
 
 type FeedType = OmitUnrelated<Feed>
@@ -70,44 +70,44 @@ assertTypeExtends<FeedType, types.Feed>()
 assertTypeExtends<types.Feed, FeedType>()
 
 let FeedItemsTable = tableSchema({
-	name: 'feedItems',
-	columns: [
-		{ name: 'feedId', type: 'string', isIndexed: true },
-		{ name: 'pubDate', type: 'number', isIndexed: true },
-		{ name: 'guid', type: 'string', isIndexed: true },
-		{ name: 'title', type: 'string' },
-		{ name: 'description', type: 'string' },
-		{ name: 'author', type: 'string', isOptional: true },
-		{ name: 'category', type: 'string', isOptional: true },
-		{ name: 'link', type: 'string' },
-		{ name: 'enclosure', type: 'string', isOptional: true },
-		{ name: 'updatedAt', type: 'number' },
-		{ name: 'createdAt', type: 'number' },
-		{ name: 'deleted', type: 'boolean' }
-	]
+  name: 'feedItems',
+  columns: [
+    { name: 'feedId', type: 'string', isIndexed: true },
+    { name: 'pubDate', type: 'number', isIndexed: true },
+    { name: 'guid', type: 'string', isIndexed: true },
+    { name: 'title', type: 'string' },
+    { name: 'description', type: 'string' },
+    { name: 'author', type: 'string', isOptional: true },
+    { name: 'category', type: 'string', isOptional: true },
+    { name: 'link', type: 'string' },
+    { name: 'enclosure', type: 'string', isOptional: true },
+    { name: 'updatedAt', type: 'number' },
+    { name: 'createdAt', type: 'number' },
+    { name: 'deleted', type: 'boolean' }
+  ]
 })
 
 export class FeedItem extends Model {
-	static table = 'feedItems'
+  static table = 'feedItems'
 
-	@field('feedId') feedId!: string
-	@field('pubDate') pubDate!: number
-	@field('guid') guid!: string
-	@text('title') title!: string
-	@text('description') description!: string
-	@text('author') author?: string
-	@text('category') category?: string
-	@text('link') link!: string
-	@json('enclosure', x => x) enclosure?: {
-		type: string
-		url: string
-		length: number
-	}
-	@field('updatedAt') updatedAt!: number
-	@field('createdAt') createdAt!: number
-	@field('deleted') deleted!: boolean
+  @field('feedId') feedId!: string
+  @field('pubDate') pubDate!: number
+  @field('guid') guid!: string
+  @text('title') title!: string
+  @text('description') description!: string
+  @text('author') author?: string
+  @text('category') category?: string
+  @text('link') link!: string
+  @json('enclosure', x => x) enclosure?: {
+    type: string
+    url: string
+    length: number
+  }
+  @field('updatedAt') updatedAt!: number
+  @field('createdAt') createdAt!: number
+  @field('deleted') deleted!: boolean
 
-	@relation('feed', 'feedId') relFeed?: Feed
+  @relation('feed', 'feedId') relFeed?: Feed
 }
 
 type FeedItemType = OmitUnrelated<FeedItem>
@@ -116,30 +116,30 @@ assertTypeExtends<FeedItemType, types.FeedItem>()
 assertTypeExtends<types.FeedItem, FeedItemType>()
 
 let UserSubscriptionsTable = tableSchema({
-	name: 'userSubscriptions',
-	columns: [
-		{ name: 'feedId', type: 'string', isIndexed: true },
-		{ name: 'userId', type: 'string' },
-		{ name: 'url', type: 'string' },
-		{ name: 'requestedFrequency', type: 'number' },
-		{ name: 'createdAt', type: 'number' },
-		{ name: 'updatedAt', type: 'number' },
-		{ name: 'deleted', type: 'boolean' }
-	]
+  name: 'userSubscriptions',
+  columns: [
+    { name: 'feedId', type: 'string', isIndexed: true },
+    { name: 'userId', type: 'string' },
+    { name: 'url', type: 'string' },
+    { name: 'requestedFrequency', type: 'number' },
+    { name: 'createdAt', type: 'number' },
+    { name: 'updatedAt', type: 'number' },
+    { name: 'deleted', type: 'boolean' }
+  ]
 })
 
 export class UserSubscription extends Model {
-	static table = 'userSubscriptions'
+  static table = 'userSubscriptions'
 
-	@field('feedId') feedId!: string
-	@field('userId') userId!: string
-	@field('url') url!: string
-	@field('requestedFrequency') requestedFrequency!: number
-	@field('updatedAt') updatedAt!: number
-	@field('createdAt') createdAt!: number
-	@field('deleted') deleted!: boolean
+  @field('feedId') feedId!: string
+  @field('userId') userId!: string
+  @field('url') url!: string
+  @field('requestedFrequency') requestedFrequency!: number
+  @field('updatedAt') updatedAt!: number
+  @field('createdAt') createdAt!: number
+  @field('deleted') deleted!: boolean
 
-	@relation('feed', 'feedId') relFeed?: Feed
+  @relation('feed', 'feedId') relFeed?: Feed
 }
 
 type UserSubscriptionType = OmitUnrelated<UserSubscription>
@@ -148,26 +148,26 @@ assertTypeExtends<UserSubscriptionType, types.UserSubscription>()
 assertTypeExtends<types.UserSubscription, UserSubscriptionType>()
 
 let UserFeedItemReadsTable = tableSchema({
-	name: 'userFeedItemReads',
-	columns: [
-		{ name: 'userId', type: 'string' },
-		{ name: 'feedItemId', type: 'string', isIndexed: true },
-		{ name: 'deleted', type: 'boolean' },
-		{ name: 'createdAt', type: 'number' },
-		{ name: 'updatedAt', type: 'number' }
-	]
+  name: 'userFeedItemReads',
+  columns: [
+    { name: 'userId', type: 'string' },
+    { name: 'feedItemId', type: 'string', isIndexed: true },
+    { name: 'deleted', type: 'boolean' },
+    { name: 'createdAt', type: 'number' },
+    { name: 'updatedAt', type: 'number' }
+  ]
 })
 
 export class UserFeedItemRead extends Model {
-	static table = 'userFeedItemReads'
+  static table = 'userFeedItemReads'
 
-	@field('userId') userId!: string
-	@field('feedItemId') feedItemId!: string
-	@field('deleted') deleted!: boolean
-	@field('createdAt') createdAt!: number
-	@field('updatedAt') updatedAt!: number
+  @field('userId') userId!: string
+  @field('feedItemId') feedItemId!: string
+  @field('deleted') deleted!: boolean
+  @field('createdAt') createdAt!: number
+  @field('updatedAt') updatedAt!: number
 
-	@relation('feedItem', 'feedItemId') relFeedItem!: FeedItem
+  @relation('feedItem', 'feedItemId') relFeedItem!: FeedItem
 }
 
 type UserFeedItemReadType = OmitUnrelated<UserFeedItemRead>
@@ -176,88 +176,88 @@ assertTypeExtends<UserFeedItemReadType, types.UserFeedItemRead>()
 assertTypeExtends<types.UserFeedItemRead, UserFeedItemReadType>()
 
 export let schema = appSchema({
-	version: 1,
-	tables: [FeedsTable, FeedItemsTable, UserSubscriptionsTable, UserFeedItemReadsTable]
+  version: 1,
+  tables: [FeedsTable, FeedItemsTable, UserSubscriptionsTable, UserFeedItemReadsTable]
 })
 
 export let migrations = schemaMigrations({
-	migrations: [
-		// We'll add migration definitions here later
-	]
+  migrations: [
+    // We'll add migration definitions here later
+  ]
 })
 
 import LokiJSAdapter from '@nozbe/watermelondb/adapters/lokijs'
 
 const adapter = new LokiJSAdapter({
-	schema,
-	// (Comment out migrations for development purposes)
-	migrations,
-	useWebWorker: false,
-	useIncrementalIndexedDB: true,
-	dbName: 'cloudyRSS',
+  schema,
+  // (Comment out migrations for development purposes)
+  migrations,
+  useWebWorker: false,
+  useIncrementalIndexedDB: true,
+  dbName: 'cloudyRSS',
 
-	// --- Optional, but recommended event handlers:
-	onQuotaExceededError: error => {
-		// Browser ran out of disk space -- TODO: setup event bus
-	},
-	onSetUpError: error => {
-		// Database failed to load -- TODO: setup event bus
-	},
-	extraIncrementalIDBOptions: {
-		onDidOverwrite: () => {
-			// Called when this adapter is forced to overwrite contents of IndexedDB.
-			// This happens if there's another open tab of the same app that's making changes.
-			// Try to synchronize the app now, and if user is offline, alert them that if they close this
-			// tab, some data may be lost
-		},
-		onversionchange: () => {
-			// database was deleted in another browser tab (user logged out), so we must make sure we
-			// delete it in this tab as well - usually best to just refresh the page
-			// if (checkIfUserIsLoggedIn()) {
-			window.location.reload()
-			// }
-		}
-	}
+  // --- Optional, but recommended event handlers:
+  onQuotaExceededError: error => {
+    // Browser ran out of disk space -- TODO: setup event bus
+  },
+  onSetUpError: error => {
+    // Database failed to load -- TODO: setup event bus
+  },
+  extraIncrementalIDBOptions: {
+    onDidOverwrite: () => {
+      // Called when this adapter is forced to overwrite contents of IndexedDB.
+      // This happens if there's another open tab of the same app that's making changes.
+      // Try to synchronize the app now, and if user is offline, alert them that if they close this
+      // tab, some data may be lost
+    },
+    onversionchange: () => {
+      // database was deleted in another browser tab (user logged out), so we must make sure we
+      // delete it in this tab as well - usually best to just refresh the page
+      // if (checkIfUserIsLoggedIn()) {
+      window.location.reload()
+      // }
+    }
+  }
 })
 
 export let db = new Database({
-	adapter,
-	modelClasses: [Feed, FeedItem, UserSubscription, UserFeedItemRead]
+  adapter,
+  modelClasses: [Feed, FeedItem, UserSubscription, UserFeedItemRead]
 })
 
 export let m = {
-	feeds: db.collections.get<Feed>('feeds'),
-	feedItems: db.collections.get<FeedItem>('feedItems'),
-	userSubscriptions: db.collections.get<UserSubscription>('userSubscriptions'),
-	userFeedItemReads: db.collections.get<UserFeedItemRead>('userFeedItemReads')
+  feeds: db.collections.get<Feed>('feeds'),
+  feedItems: db.collections.get<FeedItem>('feedItems'),
+  userSubscriptions: db.collections.get<UserSubscription>('userSubscriptions'),
+  userFeedItemReads: db.collections.get<UserFeedItemRead>('userFeedItemReads')
 }
 
 const syncEndpoint = import.meta.env.VITE_SYNC_ENDPOINT
 
 export async function dbSync() {
-	await synchronize({
-		database: db,
-		pullChanges: async ({ lastPulledAt, schemaVersion, migration }) => {
-			const urlParams = `last_pulled_at=${lastPulledAt}&schema_version=${schemaVersion}&migration=${encodeURIComponent(
-				JSON.stringify(migration)
-			)}`
-			const response = await fetch(`${syncEndpoint}/sync?${urlParams}`)
-			if (!response.ok) {
-				throw new Error(await response.text())
-			}
+  await synchronize({
+    database: db,
+    pullChanges: async ({ lastPulledAt, schemaVersion, migration }) => {
+      const urlParams = `last_pulled_at=${lastPulledAt}&schema_version=${schemaVersion}&migration=${encodeURIComponent(
+        JSON.stringify(migration)
+      )}`
+      const response = await fetch(`${syncEndpoint}/sync?${urlParams}`)
+      if (!response.ok) {
+        throw new Error(await response.text())
+      }
 
-			const { changes, timestamp } = await response.json()
-			return { changes, timestamp }
-		},
-		pushChanges: async ({ changes, lastPulledAt }) => {
-			const response = await fetch(`${syncEndpoint}/sync?last_pulled_at=${lastPulledAt}`, {
-				method: 'POST',
-				body: JSON.stringify(changes)
-			})
-			if (!response.ok) {
-				throw new Error(await response.text())
-			}
-		},
-		migrationsEnabledAtVersion: 1
-	})
+      const { changes, timestamp } = await response.json()
+      return { changes, timestamp }
+    },
+    pushChanges: async ({ changes, lastPulledAt }) => {
+      const response = await fetch(`${syncEndpoint}/sync?last_pulled_at=${lastPulledAt}`, {
+        method: 'POST',
+        body: JSON.stringify(changes)
+      })
+      if (!response.ok) {
+        throw new Error(await response.text())
+      }
+    },
+    migrationsEnabledAtVersion: 1
+  })
 }
--- packages/frontend/src/service-worker.ts
+++ packages/frontend/src/service-worker.ts
@@ -1,66 +1,66 @@
 /// <reference types="@sveltejs/kit" />
 /// <reference types="@types/serviceworker" />
 
-import { build, files, version } from '$service-worker';
+import { build, files, version } from '$service-worker'
 
 // Create a unique cache name for this deployment
-const CACHE_PREFIX = `assets`;
-const CACHE = `${CACHE_PREFIX}-${version}`;
+const CACHE_PREFIX = `assets`
+const CACHE = `${CACHE_PREFIX}-${version}`
 
 const ASSETS = [
-	...build, // the app itself
-	...files // everything in `static`
-];
+  ...build, // the app itself
+  ...files // everything in `static`
+]
 
 self.addEventListener('install', (event: ExtendableEvent) => {
-	// Create a new cache and add all files to it
-	async function addFilesToCache() {
-		const cache = await caches.open(CACHE);
-		await cache.addAll(ASSETS);
-	}
+  // Create a new cache and add all files to it
+  async function addFilesToCache() {
+    const cache = await caches.open(CACHE)
+    await cache.addAll(ASSETS)
+  }
 
-	event.waitUntil(addFilesToCache());
-});
+  event.waitUntil(addFilesToCache())
+})
 
 self.addEventListener('activate', (event: ExtendableEvent) => {
-	// Remove previous cached data from disk
-	async function deleteOldCaches() {
-		for (const key of await caches.keys()) {
-			if (key.startsWith(CACHE_PREFIX) && key !== CACHE) await caches.delete(key);
-		}
-	}
+  // Remove previous cached data from disk
+  async function deleteOldCaches() {
+    for (const key of await caches.keys()) {
+      if (key.startsWith(CACHE_PREFIX) && key !== CACHE) await caches.delete(key)
+    }
+  }
 
-	event.waitUntil(deleteOldCaches());
-});
+  event.waitUntil(deleteOldCaches())
+})
 
 self.addEventListener('fetch', (event: FetchEvent) => {
-	// ignore POST requests etc
-	if (event.request.method !== 'GET') return;
+  // ignore POST requests etc
+  if (event.request.method !== 'GET') return
 
-	async function respond() {
-		const url = new URL(event.request.url);
-		const cache = await caches.open(CACHE);
+  async function respond() {
+    const url = new URL(event.request.url)
+    const cache = await caches.open(CACHE)
 
-		// `build`/`files` can always be served from the cache
-		if (ASSETS.includes(url.pathname)) {
-			return (await cache.match(url.pathname))!;
-		}
+    // `build`/`files` can always be served from the cache
+    if (ASSETS.includes(url.pathname)) {
+      return (await cache.match(url.pathname))!
+    }
 
-		// for everything else, try the network first, but
-		// fall back to the cache if we're offline
-		try {
-			const response = await fetch(event.request);
+    // for everything else, try the network first, but
+    // fall back to the cache if we're offline
+    try {
+      const response = await fetch(event.request)
 
-			if (response.status === 200) {
-				cache.put(event.request, response.clone());
-			}
+      if (response.status === 200) {
+        cache.put(event.request, response.clone())
+      }
 
-			return response;
-		} catch {
-			let cacheFallback = await cache.match(event.request);
-			return cacheFallback!;
-		}
-	}
+      return response
+    } catch {
+      let cacheFallback = await cache.match(event.request)
+      return cacheFallback!
+    }
+  }
 
-	event.respondWith(respond());
-});
+  event.respondWith(respond())
+})
